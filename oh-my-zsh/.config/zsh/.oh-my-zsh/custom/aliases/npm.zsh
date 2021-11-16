78805fb317fc4a79259bd6efd9384e8070f31aeb
Miquel de Domingo <miquelddg@gmail.com>
2021-11-16 14:34:09 +0100

    feat: updated npm aliases

--- oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/npm.zsh
+++ oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/npm.zsh
@@ -2,15 +2,11 @@
 #	Package managing
 alias nig="sudo npm install --global"
 alias ni="npm install"
-alias nis="npm install --save"
-alias nid="npm install --save-dev"
-alias nxi="npx install"
+alias nis="npm install --save-dev"
 alias nit="npm install-test"
-alias nits="npm install-test --save"
-alias nitd="npm install-test --save-dev"
+alias nits="npm install-test --save-dev"
 alias nu="npm uninstall"
-alias nus="npm uninstall --save"
-alias nud="npm uninsatll --save-dev"
+alias nus="npm uninstall --save-dev"
 alias nup="npm upgrade"
 alias nud="npm udpate"
 alias nod="npm outdated"
@@ -25,24 +21,9 @@ alias nlsg="npm list --global"
 alias nrrd="nis react-router-dom"
 alias nrpt="nis prop-types"
 #	@General
-alias ibabel="nisd @babel/core @babel/preset-env @babel/node"
+alias ibabel="nis @babel/core @babel/preset-env @babel/node"
 
 # Package specific
-#	React
-cra() {
-	if [ -z $1 ]; then
-		create-react-app .
-	else
-		create-react-app $1
-	fi
-}
-rhoc() {
-	if [ -z $1 ]; then
-		mkdir -p hoc && touch hoc/Wrapper.jsx
-	else
-		mkdir -p hoc && touch hoc/$1.jsx
-	fi
-}
 #	json-server
 alias genjs="mkdir -p data && echo \"{\n\n}\" >> data/db.json"
 alias genjscfg="mkdir -p data && echo \"{\n\n}\" >> data/config.json"
