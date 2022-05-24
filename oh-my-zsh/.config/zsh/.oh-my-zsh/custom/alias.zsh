34be8351093674ffd2007a26d6b5b13c53233363
Miquel de Domingo <miquel.dedomingo@xing.com>
2022-05-24 13:21:57 +0200

    fix: source work alias

--- oh-my-zsh/.config/zsh/.oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/.config/zsh/.oh-my-zsh/custom/alias.zsh
@@ -1,3 +1,4 @@
 # Alias exports
 source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/configuration.zsh
 source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/npm.zsh
+source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/work.zsh
