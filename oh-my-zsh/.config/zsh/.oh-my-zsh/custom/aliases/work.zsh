64fa95928a963e0bfdf396dfbe0c1f6018bc626b
Miquel de Domingo <miquel.dedomingo@xing.com>
2022-06-13 08:32:16 +0200

    feat: minor changes

--- oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/work.zsh
+++ oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/work.zsh
@@ -1,9 +1,11 @@
 # Fast access to common directories
 alias docs="$HOME/Documents/Work"
 alias jf="$HOME/Documents/jobs-frontend"
+alias master='git checkout master && gl'
 
 # gh cli commands
 acceptance() { gh pr comment $1 --body '[run-acceptance-tests]' }
+qaimage() { gh pr comment $1 --body '[build-qa-docker-image]' }
 imgaccept() { 
   echo -e "[run-acceptance-tests]\n[build-qa-docker-image]" > prmsg
   export prmsg=$(cat prmsg); gh pr comment $1 --body "${prmsg}"
