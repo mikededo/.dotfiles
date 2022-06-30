d01a86cd36d9539753c3fd92667ff6959af55a4e
Miquel de Domingo <miquelddg@gmail.com>
2022-06-30 19:56:44 +0200

    feat: added go specific tabsize

--- nvim/.config/nvim/lua/custom/configs.lua
+++ nvim/.config/nvim/lua/custom/configs.lua
@@ -1,5 +1,11 @@
 vim.api.nvim_set_option('guicursor', 'i:block')
 vim.cmd('set incsearch')
 vim.cmd('set noshowmode')
+vim.cmd('set tabstop=4')
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'go' },
+  command = 'setlocal tabstop=4 shiftwidth=4 softtabstop=4',
+})
 
 return {}
