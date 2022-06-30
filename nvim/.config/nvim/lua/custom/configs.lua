9907856072a7cde41bcad4572be4f67b16d0adbc
Miquel de Domingo <miquelddg@gmail.com>
2022-06-30 20:00:35 +0200

    fix: git commit message cursor position

--- nvim/.config/nvim/lua/custom/configs.lua
+++ nvim/.config/nvim/lua/custom/configs.lua
@@ -4,6 +4,11 @@ vim.cmd('set noshowmode')
 vim.cmd('set tabstop=4')
 
 vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'gitcommit' },
+  command = "call setpos('.', [0, 1, 1, 0])",
+})
+
+vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'go' },
   command = 'setlocal tabstop=4 shiftwidth=4 softtabstop=4',
 })
