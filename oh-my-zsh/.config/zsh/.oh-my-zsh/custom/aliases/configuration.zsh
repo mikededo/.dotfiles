947f0cfb4ea86b1d5d6d74f311bfefb0e8a14473
Miquel de Domingo <miquelddg@gmail.com>
2022-08-30 11:29:26 +0200

    fix: rm unused alias file

--- oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/configuration.zsh
+++ oh-my-zsh/.config/zsh/.oh-my-zsh/custom/aliases/configuration.zsh
@@ -2,16 +2,18 @@
 alias ohmyconfig="vim $HOME/.config/zsh/.oh-my-zsh/oh-my-zsh.sh"
 alias p10kconfig="vim $HOME/.config/zsh/.p10k.zsh"
 alias zshconfig="vim $HOME/.config/zsh/.zshrc"
-alias chadrc="nvim $HOME/.config/nvim/lua/custom/chadrc.lua"
+alias chadrc="vim $HOME/.config/nvim/lua/custom/chadrc.lua"
 alias gitconfig="vim $HOME/.gitconfig"
 alias kittyconfig="vim $HOME/.config/kitty/kitty.conf"
 alias dotman="$HOME/.dotfiles"
+alias nvimdir="$HOME/.dotfiles/nvim/.config/nvim/lua/custom"
 alias updzsh="exec zsh"
 
 # Alias files
-alias cfgalias="vim $HOME/.config/zsh/.oh-my-zsh/custom/aliases/configuration.zsh"
-alias npmalias="vim $HOME/.config/zsh/.oh-my-zsh/custom/aliases/npm.zsh"
-alias workalias="vim $HOME/.config/zsh/.oh-my-zsh/custom/aliases/work.zsh"
+ALIAS_DIR="$HOME/.config/zsh/.oh-my-zsh/custom/aliases"
+alias cfgalias="vim $ALIAS_DIR/configuration.zsh"
+alias utilalias="vim $ALIAS_DIR/utils.zsh"
+alias workalias="vim $ALIAS_DIR/work.zsh"
 
 # File manipulation
 mkcd() { mkdir $1 && cd $1 }
