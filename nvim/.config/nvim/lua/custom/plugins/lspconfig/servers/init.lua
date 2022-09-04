891dcb38b59e7540ba6af1bc3d8ae1d5e4c1e6be
Miquel de Domingo <miquelddg@gmail.com>
2022-09-04 09:51:08 +0200

    fix: attach tailwindcss server

--- nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/init.lua
@@ -1,12 +1,14 @@
+local servers = 'custom.plugins.lspconfig.servers'
+local ts = require(servers .. '.typescript')
+local jsonls = require(servers .. '.jsonls')
+local go = require(servers .. '.go')
+
 return {
   attach = function(on_attach)
-    require('custom.plugins.lspconfig.servers.typescript').setup_diagnostics(
-      on_attach
-    )
-    require('custom.plugins.lspconfig.servers.typescript').setup_tsserver(
-      on_attach
-    )
-    require('custom.plugins.lspconfig.servers.jsonls').setup_jsonls(on_attach)
-    require('custom.plugins.lspconfig.servers.go').setup(on_attach)
+    ts.setup_diagnostics(on_attach)
+    ts.setup_tsserver(on_attach)
+    ts.setup_tailwindcss(on_attach)
+    jsonls.setup_jsonls(on_attach)
+    go.setup(on_attach)
   end,
 }
