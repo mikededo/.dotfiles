40de9c899e6e84bf41fe96d762fc188f02631165
Miquel de Domingo <miquelddg@gmail.com>
2022-09-07 12:00:25 +0200

    fix: add capabilities to jsonls

--- nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/jsonls.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/jsonls.lua
@@ -5,10 +5,13 @@ end
 
 local M = {}
 local nvim_lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 M.setup_jsonls = function(on_attach)
   nvim_lsp.jsonls.setup({
     on_attach,
+    capabilities = capabilities,
     settings = {
       json = {
         schemas = schemastore.json.schemas(),
