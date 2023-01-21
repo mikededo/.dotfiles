6827f0174c4d2a8073752c6afe23502d382647d4
Miquel de Domingo <miquelddg@gmail.com>
2023-01-21 17:23:33 +0100

    feat: add `golang-lint-ls` lang server

--- nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/go.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig/servers/go.lua
@@ -1,25 +1,35 @@
 local nvim_lsp = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-return {
-  setup = function(on_attach)
-    nvim_lsp.gopls.setup({
-      cmd = { 'gopls' },
-      on_attach = on_attach,
-      capabilities = capabilities,
-      settings = {
-        gopls = {
-          experimentalPostfixCompletions = true,
-          analyses = {
-            unusedparams = true,
-            shadow = true,
-          },
-          staticcheck = true,
+local setup_go = function(on_attach)
+  nvim_lsp.gopls.setup({
+    cmd = { 'gopls' },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      gopls = {
+        experimentalPostfixCompletions = true,
+        analyses = {
+          unusedparams = true,
+          shadow = true,
         },
+        staticcheck = true,
       },
-      init_options = {
-        usePlaceholders = true,
-      },
-    })
-  end,
+    },
+    init_options = {
+      usePlaceholders = true,
+    },
+  })
+end
+
+local setup_golang_lint = function(on_attach)
+  nvim_lsp.golangci_lint_ls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
+return {
+  setup_go = setup_go,
+  setup_golang_lint = setup_golang_lint,
 }
