beb288e7b7051931c21cd8ff683c3da025dd74ab
Miquel de Domingo <miquelddg@gmail.com>
2023-02-12 19:52:12 +0100

    feat(alias): correct `reset_gpg` command to ovewrite file

--- fish/.config/fish/conf.d/aliases/utils.fish
+++ fish/.config/fish/conf.d/aliases/utils.fish
@@ -30,4 +30,4 @@ alias fp0="yarn list --depth 0 | grep $1"
 
 # other
 ## avoids having to keep entering the passphrase to sign the commits
-alias reset_gpg="echo 'pinentry-program /opt/homebrew/bin/pinentry-mac' >> ~/.gnupg/gpg-agent.conf && killall gpg-agent"
+alias reset_gpg="echo 'pinentry-program /opt/homebrew/bin/pinentry-mac' > ~/.gnupg/gpg-agent.conf && killall gpg-agent"
