270edfe94e45df7d468faf5730a966ec5aca5fad
Miquel de Domingo <miquelddg@gmail.com>
2023-05-23 17:27:20 +0200

    feat(core)!: removed powerlevel10k & oh-my-zsh

--- oh-my-zsh/.config/zsh/.oh-my-zsh/custom/alias.zsh
+++
@@ -1,4 +0,0 @@
-# Alias exports
-source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/configuration.zsh
-source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/utils.zsh
-source $HOME/.config/zsh/.oh-my-zsh/custom/aliases/work.zsh
--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env zsh
 
-OHMYZSH_CONFIG=$HOME/.config/zsh/.oh-my-zsh
-OHMYZSH_THEMES=$OHMYZSH_CONFIG/themes
-OHMYZSH_PLUGINS=$OHMYZSH_CONFIG/custom/plugins
-
 # Check mandatory applications
 [ ! -x "$(command -v zsh)" ] && echo "ERROR: zsh is not installed in the system" && exit -1
 [ ! -x "$(command -v fish)" ] && echo "ERROR: fish is not installed in the system" && exit -1
@@ -17,8 +13,6 @@ packages=(
   git
   kitty
   nvim
-  oh-my-zsh
-  powerlevel10k
   zsh
   fish
   starship
@@ -35,19 +29,6 @@ stow_it() {
 git_clone() {
   git clone "https://github.com/$1" $2
 }
-
-# Install zsh and its plugins
-echo "> Installing oh-my-zsh"
-
-[ ! -d $HOME/.config/zsh/.oh-my-zsh ] && git_clone ohmyzsh/ohmyzsh $HOME/.config/zsh/.oh-my-zsh
-
-[ ! -d $OHMYZSH_PLUGINS/zsh-autosuggestions ] && git_clone zsh-users/zsh-autosuggestions $OHMYZSH_PLUGINS/zsh-autosuggestions
-[ ! -d $OHMYZSH_PLUGINS/zsh-syntax-highlighting ] && git_clone zsh-users/zsh-syntax-highlighting $OHMYZSH_PLUGINS/zsh-syntax-highlighting
-[ ! -d $OHMYZSH_PLUGINS/zsh-better-npm-completion ] && git_clone lukechilds/zsh-better-npm-completion $OHMYZSH_PLUGINS/zsh-better-npm-completion
-
-echo "> Installing powerlevel10k"
-
-[ ! -d $OHMYZSH_THEMES/powerlevel10k ] && git_clone romkatv/powerlevel10k $OHMYZSH_THEMES/powerlevel10k
 
 echo "> Adding NvChad\n"
 
