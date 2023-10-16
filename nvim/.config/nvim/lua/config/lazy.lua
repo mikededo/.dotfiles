6148cdbb00a9bd7c2b562aef2779c3f86f45a954
Miquel de Domingo <miquelddg@gmail.com>
2023-10-16 10:38:24 +0200

    feat(nvim): lazyvim 10.x improvements

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -9,15 +9,13 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require('lazy').setup({
   spec = {
     -- add LazyVim and import its plugins
-    { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = 'lazyvim.plugins.extras.lang.typescript' },
-    { import = 'lazyvim.plugins.extras.lang.json' },
-    { import = 'lazyvim.plugins.extras.lang.tailwind' },
-    { import = 'lazyvim.plugins.extras.lang.rust' },
-    { import = 'lazyvim.plugins.extras.linting.eslint' },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
-    { import = 'plugins' },
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -26,7 +24,6 @@ require('lazy').setup({
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { 'tokyonight' } },
   checker = { enabled = true }, -- automatically check for plugin updates
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+return {
+  "stevearc/conform.nvim",
+  optional = false,
+  enabled = true,
+}
