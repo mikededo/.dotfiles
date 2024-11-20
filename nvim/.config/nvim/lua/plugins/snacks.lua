5e22f9ac94efa463731ae56abcc047f6f960868a
Miquel de Domingo <miquelddg@gmail.com>
2024-11-20 09:14:12 +0100

    feat(nvim): remove `dashboard.nvim`

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,27 @@
+local uv = vim.uv or vim.loop
+
 return {
   'folke/snacks.nvim',
   ---@type snacks.Config
   opts = {
     notifier = { enabled = false },
     notify = { enabled = false },
+    dashboard = {
+      sections = {
+        {
+          header = [[
+███╗   ██╗██╗   ██╗██╗███╗   ███╗
+████╗  ██║██║   ██║██║████╗ ████║
+██╔██╗ ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+          padding = 1,
+        },
+        { title = 'Shortcuts', padding = 1 },
+        { section = 'keys' },
+        { section = 'startup', padding = { 0, 2 } },
+      },
+    },
   },
 }
