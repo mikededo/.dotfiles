30ea0f92590de971271c3c19d7a26a15f8601104
Miquel de Domingo <miquelddg@gmail.com>
2025-06-01 08:44:33 +0200

    feat(nvim): a/i around js template

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,15 +1,38 @@
 return {
-  'echasnovski/mini.ai',
-  opts = function()
-    local ai = require('mini.ai')
-    return {
-      n_lines = 500,
-      custom_textobjects = {
-        v = ai.gen_spec.treesitter({
-          a = { '@assignment.outer' },
-          i = { '@assignment.inner' },
-        }),
+  {
+    'echasnovski/mini.ai',
+    opts = function()
+      local ai = require('mini.ai')
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          v = ai.gen_spec.treesitter({
+            a = { '@assignment.outer' },
+            i = { '@assignment.inner' },
+          }),
+        },
+      }
+    end,
+  },
+  {
+    'echasnovski/mini.surround',
+    version = '*',
+    keys = function(_, keys)
+      local mappings = {
+        { 'j', desc = 'JS template', mode = { 'n', 'v' } },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      custom_surroundings = {
+        ['j'] = {
+          input = { '`%${().-()}`' },
+          output = { left = '`${', right = '}`' },
+        },
       },
-    }
-  end,
+    },
+  },
 }
