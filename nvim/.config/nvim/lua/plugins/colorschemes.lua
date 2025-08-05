c4fbadb61bfc4fd049dc72d7e7fda67e9da78f51
Miquel de Domingo <miquelddg@gmail.com>
2025-08-05 10:45:32 +0200

    fix(nvim): revert catpuccin colors

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -35,6 +35,42 @@ return {
 
           -- WhichKey
           WhichKeyBorder = { fg = C.peach, bg = C.mantle },
+
+          -- There was a change of colors in here, which I dislike. This
+          -- reverts some of them: https://github.com/catppuccin/nvim/pull/804
+          ['@variable.member'] = { fg = C.lavender },
+          ['@module'] = {
+            fg = C.lavender,
+          },
+          ['@string.special.url'] = {
+            fg = C.rosewater,
+            style = { 'italic', 'underline' },
+          },
+          ['@type.builtin'] = {
+            fg = C.yellow,
+          },
+          ['@property'] = { fg = C.lavender },
+          ['@constructor'] = { fg = C.sapphire },
+          ['@keyword.operator'] = { link = 'Operator' },
+          ['@keyword.directive.css'] = { link = 'Keyword' },
+          ['@keyword.export'] = { fg = C.sky },
+          ['@tag'] = { fg = C.mauve },
+          ['@tag.attribute'] = {
+            fg = C.teal,
+          },
+          ['@property.css'] = { fg = C.lavender },
+          ['@property.id.css'] = { fg = C.blue },
+          ['@type.tag.css'] = { fg = C.mauve },
+          ['@string.plain.css'] = { fg = C.peach },
+          ['@constructor.lua'] = { fg = C.flamingo },
+          ['@property.typescript'] = {
+            fg = C.lavender,
+          },
+          ['@constructor.typescript'] = { fg = C.lavender },
+          ['@constructor.tsx'] = { fg = C.lavender },
+          ['@tag.attribute.tsx'] = {
+            fg = C.teal,
+          },
         }
       end,
     },
