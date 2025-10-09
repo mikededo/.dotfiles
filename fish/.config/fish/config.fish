032be1dbc9c965e7cd4e27558e6f5d9049ac7dda
Miquel de Domingo <miquelddg@gmail.com>
2025-10-09 09:19:48 +0200

    feat(fish): add custom theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,10 +8,6 @@ set -gx PATH "$VOLTA_HOME/bin" $PATH
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
 function fish_right_prompt_loading_indicator -a last_prompt
     echo -n "$last_prompt" | sed -r 's/\x1B\[[0-9;]*[JKmsu]//g' | read -zl uncolored_last_prompt
     echo -n (set_color brblack)"$uncolored_last_prompt"(set_color normal)
