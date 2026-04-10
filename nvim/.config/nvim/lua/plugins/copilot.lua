f3659e6cbc8ef9912b5d1eb1281939ef3a03478f
Miquel de Domingo <miquelddg@gmail.com>
2026-04-10 09:44:38 +0100

    fix(nvim): make copilot use default `fnm` version

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'zbirenbaum/copilot.lua',
+    opts = {
+      copilot_node_command = vim.fn.expand('$HOME')
+        .. '/.local/share/fnm/aliases/default/bin/node',
+    },
+  },
+}
