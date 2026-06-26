de8e9aa71a723b4a0b2de6062a94168065fa0e1b
Miquel de Domingo <miquelddg@gmail.com>
2026-06-26 09:50:19 +0200

    fix(nvim): correct svelte vtsls config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -89,6 +89,18 @@ return {
         },
         vtsls = {
           settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = 'typescript-svelte-plugin',
+                    location = vim.fn.stdpath('data')
+                      .. '/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin',
+                    enableForWorkspaceTypeScriptVersions = true,
+                  },
+                },
+              },
+            },
             typescript = {
               tsserver = {
                 maxTsServerMemory = 8192,
